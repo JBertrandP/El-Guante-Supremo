dca2c79ca7b05cbfa40661ce2e4678f9817d3ad4
karimeli <141066459+karimeli@users.noreply.github.com>
2025-06-19 08:00:44 -0600

    homepage

---
+++ mobile/home_page.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import { View, Text, TouchableOpacity, Image, StyleSheet } from 'react-native';
+
+const homePage = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}>Bienvenido</Text>
+      <Image
+    
+        style={styles.image}
+      />
+      <Text style={styles.subHeader}>Descubre cómo funciona</Text>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>O prueba la traducción rápida</Text>
+      </TouchableOpacity>
+
+      <View style={styles.footer}>
+        <TouchableOpacity style={styles.footerButton}>
+          <Text style={styles.footerButtonText}>Inicio</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.footerButton}>
+          <Text style={styles.footerButtonText}>Traductor</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.footerButton}>
+          <Text style={styles.footerButtonText}>Alfabeto</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.footerButton}>
+          <Text style={styles.footerButtonText}>Diccionario</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#33AAEE',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  header: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#FFFFFF',
+    marginBottom: 20,
+  },
+  subHeader: {
+    fontSize: 18,
+    color: '#FFFFFF',
+    marginBottom: 20,
+  },
+  image: {
+    width: 100,
+    height: 100,
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: '#4996C3',
+    padding: 10,
+    borderRadius: 5,
+    marginBottom: 20,
+  },
+  buttonText: {
+    color: '#FFFFFF',
+    fontSize: 16,
+  },
+  footer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '100%',
+    position: 'absolute',
+    bottom: 0,
+    padding: 10,
+    backgroundColor: '#076AA3', 
+  },
+  footerButton: {
+    backgroundColor: '#054F7A',
+    padding: 10,
+    borderRadius: 5,
+  },
+  footerButtonText: {
+    color: '#FFFFFF',
+    fontSize: 14,
+  },
+});
+
+export default homePage;
