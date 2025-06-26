83df5047cae71d1702181bdc4163d89b28411813
karimeli <141066459+karimeli@users.noreply.github.com>
2025-06-26 10:08:00 -0600

    actualizacion

--- mobile/App.js
+++ mobile/App.js
@@ -1,6 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { View, StyleSheet } from 'react-native';
 import PantallaPrincipal from './views/splash_screen';
-import Login from './views/Login'; // Importa la pantalla de login
+import Login from './views/Login';
 
 export default function App() {
   const [mostrarSplash, setMostrarSplash] = useState(true);
@@ -8,10 +9,20 @@ export default function App() {
   useEffect(() => {
     const timer = setTimeout(() => {
       setMostrarSplash(false);
-    }, 3000); // 3 segundos
+    }, 3000); // 3 segundos de splash
 
     return () => clearTimeout(timer);
   }, []);
 
-  return mostrarSplash ? <PantallaPrincipal /> : <Login />;
+  return (
+    <View style={styles.container}>
+      {mostrarSplash ? <PantallaPrincipal /> : <Login />}
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
