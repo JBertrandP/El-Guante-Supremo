a65e5979c3645a1762dee13116a0196db204756f
karimeli <141066459+karimeli@users.noreply.github.com>
2025-06-30 09:12:20 -0600

    actualizacion

--- mobile/views/splash_screen.js
+++ mobile/views/splash_screen.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import { View, Image, StyleSheet } from 'react-native';
+import { View, Image, StyleSheet, Dimensions } from 'react-native';
+
+const { width, height } = Dimensions.get('window'); // Obtén el tamaño de la pantalla
 
 const PantallaPrincipal = () => {
   return (
     <View style={styles.container}>
       <Image
-        source={require('./assets/logo.png')} 
+        source={require('./assets/logo.png')}
         style={styles.icono}
         accessibilityLabel="Logo"
       />
       <Image
-        source={require('./assets/loading.gif')} 
+        source={require('./assets/loading.gif')}
         style={styles.gif}
       />
     </View>
@@ -26,13 +28,13 @@ const styles = StyleSheet.create({
     padding: 16,
   },
   icono: {
-    width: 250,
-    height: 500,
+    width: width * 0.6, // 60% del ancho de la pantalla
+    height: height * 0.25, // 25% de la altura de la pantalla
     resizeMode: 'contain',
   },
   gif: {
-    width: 300,
-    height: 600,
+    width: width * 0.8, // 80% del ancho de la pantalla
+    height: height * 0.35, // 35% de la altura de la pantalla
     resizeMode: 'contain',
     marginTop: 20,
   },
