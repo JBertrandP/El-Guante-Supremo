39c063a9205d60c0a9e527f96fd0895061cd0a83
karimeli <141066459+karimeli@users.noreply.github.com>
2025-06-30 09:28:26 -0600

    actualizacion

--- mobile/App.js
+++ mobile/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import PantallaPrincipal from './views/splash_screen';
+import Login from './views/log_in';
+import SignUp from './views/sign_up';
 export default function App() {
   return (
     <View style={styles.container}>
