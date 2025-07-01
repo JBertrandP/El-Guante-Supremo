252c3cbcef54681bc6c12dae9f0cc8ff951b8546
karimeli <141066459+karimeli@users.noreply.github.com>
2025-07-01 12:39:17 -0600

    ajuste

--- mobile/App.js
+++ mobile/App.js
@@ -1,19 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'; 
 import { View, StyleSheet } from 'react-native';
 import PantallaPrincipal from './views/splash_screen';
 import Login from './views/log_in';
-import SignUp from './views/sign_up';
 
 export default function App() {
   const [showLogin, setShowLogin] = useState(false);
 
   useEffect(() => {
-    // Esperar 5 segundos antes de mostrar el login
+   
     const timer = setTimeout(() => {
       setShowLogin(true);
-    }, 5000); // 5000ms = 5 segundos
+    }, 5000); 
 
-    return () => clearTimeout(timer); // Limpiar el timer si el componente se desmonta
+    return () => clearTimeout(timer); 
   }, []);
 
   return (
--- mobile/views/splash_screen.js
+++ mobile/views/splash_screen.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import { View, Image, StyleSheet, Dimensions } from 'react-native';
 
-const { width, height } = Dimensions.get('window'); // Obtén el tamaño de la pantalla
+const { width, height } = Dimensions.get('window'); 
 
 const PantallaPrincipal = () => {
   return (
     <View style={styles.container}>
-      <Image
-        source={require('./assets/logo.png')}
-        style={styles.icono}
-        accessibilityLabel="Logo"
-      />
-      <Image
-        source={require('./assets/loading.gif')}
-        style={styles.gif}
-      />
+      <View style={styles.innerContainer}>
+        <Image
+          source={require('./assets/logo.png')}
+          style={styles.icono}
+          accessibilityLabel="Logo"
+        />
+        <Image
+          source={require('./assets/loading.gif')}
+          style={styles.gif}
+        />
+      </View>
     </View>
   );
 };
@@ -23,21 +25,25 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#33AAEE',
-    justifyContent: 'center', // Centra en el eje vertical
-    alignItems: 'center', // Centra en el eje horizontal
+    justifyContent: 'center', 
+    alignItems: 'center',l
+  },
+  innerContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',      
+    width: width * 0.8,       
   },
   icono: {
-    width: width * 0.8, // 80% del ancho de la pantalla
-    height: height * 0.35, // 35% de la altura de la pantalla
+    width: '100%',             
+    height: height * 0.3,    
     resizeMode: 'contain',
   },
   gif: {
-    width: width * 0.6, // 60% del ancho de la pantalla
-    height: height * 0.35, // 35% de la altura de la pantalla
+    width: '100%',           
+    height: height * 0.3,      
     resizeMode: 'contain',
-    marginTop: 20, // Espacio entre el logo y el gif
+    marginTop: 0,              
   },
 });
 
 export default PantallaPrincipal;
-
