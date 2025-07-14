823e0129a8a821dcee335b83d0e854df5d0f8f98
karimeli <141066459+karimeli@users.noreply.github.com>
2025-07-14 09:40:40 -0600

    login desactivado

--- mobile/App.js
+++ mobile/App.js
@@ -1,31 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useEffect } from 'react';
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import PantallaPrincipal from './views/splash_screen'; 
-import SignUp from './views/sign_up'; 
+// import Login from './views/log_in';  
+// import SignUp from './views/sign_up'; // Desactivado temporalmente
 import Home from './views/HomePage';  
 import Alfabeto from './views/Alfabeto'; 
-import { useNavigation } from '@react-navigation/native'; 
+import BtScreen from './views/BT.JS'; // Asegúrate de que este archivo exista y esté correctamente exportado
+
 
 const Stack = createStackNavigator();
 
 export default function App() {
-
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Splash">
-        {/* Pantalla de Splash */}
         <Stack.Screen
           name="Splash"
           component={PantallaPrincipal}
           options={{ headerShown: false }}  
         />
 
-        {/* Redirige automáticamente a Home después de 3 segundos */}
+        {/* 
         <Stack.Screen
-          name="Home"
-          component={Home}
+          name="Login"
+          component={Login}
           options={{ 
             headerStyle: { backgroundColor: '#033552' },
             headerTintColor: '#FFFFFF',
@@ -40,6 +40,21 @@ export default function App() {
         <Stack.Screen
           name="SignUp"
           component={SignUp}
+          options={{ 
+            headerStyle: { backgroundColor: '#033552' },
+            headerTintColor: '#FFFFFF',
+            headerTitleStyle: {
+              fontFamily: 'Roboto', 
+              fontWeight: 'bold',
+              fontSize: 22,
+            },
+          }}
+        />
+        */}
+
+        <Stack.Screen
+          name="Home"
+          component={Home}
           options={{ 
             headerStyle: { backgroundColor: '#033552' },
             headerTintColor: '#FFFFFF',
