d86ab99809986343f2ac8f657a2b9b8a34c2a463
IWantToBeTsugurific <lalahago14@gmail.com>
2025-08-05 17:20:54 -0600

    d

--- frontend/src/components/wifi.js
+++ frontend/src/components/wifi.js
@@ -13,11 +13,10 @@ function WifiDeviceManager() {
     try {
       const res = await axios.get(`http://${ip}/status`);
       if (res.status === 200){
-        //const socket = new WebSocket(`ws://${ip}/ws/glove`);
-        const socket = new WebSocket(`wss://${API_URL}/ws/glove`);
+        const socket = new WebSocket(`wss://${API_URL}/ws/front`);
 
         socket.onopen = () => {
-          console.log("Conectado al WebSocket de glove");
+          console.log("Conectado al WebSocket");
         };
 
         socket.onmessage = (event) => {
